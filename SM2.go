93e3e6875ffd4d90be3fb7f28c339a8438c85b10
darren <darren94me@sina.com>
2022-02-07 17:54:50 +0800

    i

--- SM2.go
+++ SM2.go
@@ -9,46 +9,49 @@ import (
 
 // 公钥密码
 
-//func main1(){
-//
-//	//1 生成秘钥对
-//	privateKey, err :=sm2.GenerateKey()
-//
-//	if err !=nil{
-//		fmt.Println("秘钥对生成失败")
-//		return
-//	}
-//
-//    // 2从私钥中取出公钥
-//
-//    publicKey :=&privateKey.PublicKey
-//
-//    // 3 用公钥加密
-//    msg := []byte("darren")
-//    encrypto_msg,err :=publicKey.Encrypt( msg)
-//	if err !=nil{
-//		fmt.Println("公钥加密失败")
-//		return
-//	}else{
-//
-//		fmt.Printf("%x\n",encrypto_msg)
-//	}
-//
-//
-//	// 4 对私钥解密
-//
-//	decrypto_msg,err :=privateKey.Decrypt(encrypto_msg)
-//
-//	if err !=nil{
-//		fmt.Println("私钥解密失败")
-//		return
-//	}else{
-//		fmt.Printf("%x\n",decrypto_msg)
-//	}
-//
-//
-//
-//}
+
+
+// mod与git版本的问题
+/*
+ go.mod 是将pack存放到 /go/pkg/ 下面
+*/
+
+func main1(){
+	//1.生成密钥对
+	privateKey, err := sm2.GenerateKey()
+	if err != nil {
+		fmt.Println("秘钥生成失败!")
+		return
+	}
+
+
+	//2.从私钥中取出公钥
+	publicKey := &privateKey.PublicKey
+
+
+	//3.公钥加密
+	msg := []byte("darren")
+	encrypt_msg, err := publicKey.Encrypt(msg)
+	if err != nil {
+		fmt.Println("公钥加密失败!")
+		return
+	}else {
+		fmt.Printf("%x\n", encrypt_msg)
+	}
+
+	//4.私钥解密
+	decrypt_msg, err := privateKey.Decrypt(encrypt_msg)
+	if err != nil {
+		fmt.Println("私钥解密失败!")
+		return
+	}else {
+		fmt.Println(string(decrypt_msg))
+	}
+
+
+
+
+}
 
 
 // 验签名
@@ -64,6 +67,12 @@ func main(){
 
 }
 
+// 从文件中读取公钥和私钥
+
+
+
+
+
 
 //生成的公钥和私钥,写入文件中
 func WriteKekPairToFile(privateKeyPath, pulicKeyPath string,password []byte) error{
@@ -75,18 +84,18 @@ func WriteKekPairToFile(privateKeyPath, pulicKeyPath string,password []byte) err
 		return err
 	}
 
-    // 将私钥写入文件
+   // 将私钥写入文件
 	flag,err :=sm2.WritePrivateKeytoPem(privateKeyPath,privateKey,password)
 
-    if !flag  || err!=nil{
-    	fmt.Println("私钥写入文件失败")
-    	return err
+   if !flag  || err!=nil{
+   	fmt.Println("私钥写入文件失败")
+   	return err
 	}
 
 	// 获取公钥
 	publicKey := privateKey.Public().(*sm2.PublicKey)
-   // publicKey := privateKey.PublicKey
-   // 将公钥写入文件
+  // publicKey := privateKey.PublicKey
+  // 将公钥写入文件
 	flag,err =sm2.WritePublicKeytoPem(pulicKeyPath,publicKey,nil)
 	if !flag  || err!=nil{
 		fmt.Println("公钥写入文件失败")
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module example.com/m
 
 go 1.17
 
-require github.com/tjfoc/gmsm v1.4.1 // indirect
+require github.com/tjfoc/gmsm v1.2.5 //direct
+
+require (
+	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
