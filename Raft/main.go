dbf56ae694d877baf6bf34b249a0fe4a1a470717
darren <darren@darrendeMacBook-Pro.local>
2022-02-11 10:03:17 +0800

    raft

--- Raft/main.go
+++ Raft/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
 // 模拟选举的逻辑
 // 模拟三节点的分布式选举
@@ -71,7 +76,7 @@ func main() {
 
 // 创建节点
 func Make(me int) *Raft {
-	rf := Raft{}
+	rf := &Raft{}
 	// 编号
 	rf.me = me
 	// 0 1 2 -1 都不投票
@@ -87,12 +92,138 @@ func Make(me int) *Raft {
 	//设置任期
 	rf.setTerm(0)
 
-	rf.electCh = make(chan  bool)
+	// 通道
+	rf.electCh = make(chan bool)
+	rf.message = make(chan bool)
+	rf.heartBeat = make(chan bool)
 
+	rf.hearbeatRe = make(chan bool)
+
+	//随机种子
+	rand.Seed(time.Now().UnixNano())
+
+	// 选举逻辑
+	go rf.election()
+	// 心跳检查
+	go rf.sendLeaderHeartBeat()
+
+	return rf
+
+}
+
+func (rf *Raft) election() {
+
+	// 设置
+	var result bool
+	// 循环投票
+	for {
+		timeout := randRange(150, 900)
+
+		// 设置最后一条消息的时间
+		rf.lastMessageTime = millisecond()
+		select {
+		case <-time.After(time.Duration(timeout) * time.Millisecond):
+			fmt.Println("当前节点状态是", rf.state)
+		}
+
+		result = false
+		// 选出leader，停止循环，result=true
+		for !result {
+			// 选择leader
+			result = rf.election_ont_out(&leader)
+		}
+	}
+}
+
+func (rf *Raft) sendLeaderHeartBeat() {
 
 }
 
 func (rf *Raft) setTerm(term int) {
 
 	rf.currentTerm = term
+
+}
+
+//生产随机数
+func randRange(min, max int64) int64 {
+	return rand.Int63n(max-min) + min
+}
+
+// 获取当前时间
+func millisecond() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
+}
+
+// 选举leader
+func (rf *Raft) election_ont_out(leader Leader) bool {
+
+	// 超时时间
+	var timeout int64
+	timeout = 100
+	// 投票数量
+	var vote int64
+	// 是否开启心跳方法
+	var triggerHeartbeat bool
+	// 当前时间戳的
+	last := millisecond()
+	// 定义返回值
+
+	success := false
+
+	// 首先成为condidate状态
+	rf.mu.Lock()
+	rf.becomeCondidate()
+	rf.mu.Unlock()
+
+	// 开始选举
+	fmt.Println("start election leader")
+
+	for {
+		// 便利所有节点进行投票
+		for i := 0; i < raftCount; i++ {
+			// 遍历到不是自己 进行来票
+			if i != rf.me {
+				go func() {
+					if leader.LeaderId < 0 {
+						// 其他节点没有领导
+						rf.electCh <- true
+
+					}
+				}()
+			}
+
+		}
+
+		//设置投票数量
+		vote = 0
+		triggerHeartbeat = false
+		// 遍历所有节点进行选举
+		for i := 0; i < raftCount; i++ {
+			select {
+			case ok := <-rf.electCh:
+				if ok {
+
+				}
+			}
+		}
+
+	}
+	return success
+
+}
+
+// 修改节点为candidate
+func (rf *Raft) becomeCondidate() {
+	// 设置状态
+	rf.state = 1
+	//节点的任期+1
+	rf.setTerm(rf.currentTerm + 1)
+
+	// 设置为哪个节点投票
+	rf.votedFor = rf.me
+
+	// 是否有领导
+	rf.currentLeader = -1
+
 }
